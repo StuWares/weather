f016b95da1a972e1471d1082d64639056716b5a7
Stu Wares <stuwares@outlook.com>
2017-04-24 23:03:52 +0100

    added api call for open weather map, stuck trying to render returned ajax

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,30 @@
 /* Open Weather Map api */
+/* f0d6e0583c32177ac960f8b743c14ae0 */
 
 $(document).ready(function(){
-	
+
+	/* reqeusts location data from the browser */
+	if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    	$.ajax({
+    		url:'http://api.openweathermap.org/data/2.5/weather?',
+    		data:{
+    			appid: 'f0d6e0583c32177ac960f8b743c14ae0',
+    			lat: position.coords.latitude,
+    			lon: position.coords.longitude,
+    			callback: 'weatherData',
+    		},
+    		function(weatherData) {
+    		$("#city").html(weatherData[0].sys.name);
+    	}
+    	
+    	});
+   
+    	
+    	/* dummy output to test, puts the co-ords onto the page 
+      $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " 
+      	+ position.coords.longitude); */
+    });
+  };
 
 });
