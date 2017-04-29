f6b73d5058d36159bb65c51ed027b79dd3d333e4
Stu Wares <stuwares@outlook.com>
2017-04-29 22:29:32 +0100

    big change, completed c to f click event, removed css slider button

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -22,13 +22,25 @@ $(document).ready(function(){
     		},
     		/* render json data*/
     		success: function(response){
+    			var units = "metric";
+    			var degF = response.main.temp * 9/5 + 32;
+
     			$("#city").html("<h1>" + response.name + "</h1>");
     			$("#temp").html("<h2>" + response.main.temp.toFixed() + "&deg;C</h2>");
     			$("#details").html("<h2>" + response.weather[0].description + "</h2>");
 
-    			$("#but-box").on("click", function() {
-    			var degF = response.main.temp * 9/5 + 32;
-    			$("#temp").html("<h2>" + degF.toFixed() + "&deg;F</h2>");
+    			$("#temp").on("click", function() {
+    					
+    				if (units === "imperial") {
+    					$("#temp").html("<h2>" + response.main.temp.toFixed() + "&deg;C</h2>");
+    					units = "metric"; 
+    				}
+    				else if (units === "metric") {
+    					$("#temp").html("<h2>" + degF.toFixed() + "&deg;F</h2>");
+    					units = "imperial";
+    				}
+    				
+
     			});
 
     		},
